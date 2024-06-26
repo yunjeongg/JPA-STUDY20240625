af32c2a39bd709cd0dc75924323102564cecd0b4
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-26 10:29:51 +0900

    # 03_02. JPA Sorting 처리

--- src/test/java/com/spring/jpastudy/chap03_page/StudentPageRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap03_page/StudentPageRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -67,6 +68,31 @@ class StudentPageRepositoryTest {
         System.out.println("count = " + count);
         System.out.println();
         studentList.forEach(System.out::println);
+        System.out.println("\n\n\n");
+    }
+
+    @Test
+    @DisplayName("페이징 + 정렬")
+    void pagingAndSortTest() {
+        // gwt 패턴
+        //given - 테스트에 주어질 데이터
+        // 1. 하나의 조건으로 정렬하고 싶을 때
+        // Pageable pageInfo = PageRequest.of(0, 10, Sort.by("name").descending()); // .descending() 내림차순
+        // 1-1. PageRequest.of (1페이지, 10개, 게시글 매개값)
+        // 게시글매개값으로는 엔터티의 필드명을 적어줘야 한다.
+        // 1-2. Sort 의 기본값은 asc, .descending() 는 내림차순
+        // 김시골 2, 김시골 10 비교시 앞부터 비교하기 때문에 10이 2보다 작다.
+        // 김시골 2, 김시골 100 비교시 앞부터 비교하기 때문에 100이 2보다 작다.
+
+        // 2. 여러 조건으로 정렬하고 싶을 때
+        Pageable pageInfo = PageRequest.of(0, 10, Sort.by(Sort.Order.desc("name"), Sort.Order.asc("city")));
+
+        //when - 테스트 상황
+        Page<Student> studentPage = repository.findAll(pageInfo);
+
+        //then - 테스트 결과 단언
+        System.out.println("\n\n\n");
+        studentPage.getContent().forEach(System.out::println);
         System.out.println("\n\n\n");
     }
 }
