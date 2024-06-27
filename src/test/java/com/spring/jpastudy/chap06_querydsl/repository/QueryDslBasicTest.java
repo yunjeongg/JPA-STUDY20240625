0cd5c452d450d23161302d1c3546815bfefc7c65
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-27 11:21:34 +0900

    # 06_04. QueryDslBasic Test 01 초기 생성

---
+++ src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslBasicTest.java
@@ -0,0 +1,40 @@
+package com.spring.jpastudy.chap06_querydsl.repository;
+
+import com.spring.jpastudy.chap06_querydsl.entity.Group;
+import com.spring.jpastudy.chap06_querydsl.entity.Idol;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+// @Rollback(false)
+class QueryDslBasicTest {
+
+    IdolRepository idolRepository;
+    GroupRepository groupRepository;
+
+    @BeforeEach
+    void setUp () {
+        //given
+        Group leSserafim = new Group("르세라핌");
+        Group ive = new Group("아이브");
+
+        groupRepository.save(leSserafim);
+        groupRepository.save(ive);
+
+        Idol idol1 = new Idol("김채원", 24, leSserafim);
+        Idol idol2 = new Idol("사쿠라", 26, leSserafim);
+        Idol idol3 = new Idol("가을", 22, ive);
+        Idol idol4 = new Idol("리즈", 20, ive);
+
+        idolRepository.save(idol1);
+        idolRepository.save(idol2);
+        idolRepository.save(idol3);
+        idolRepository.save(idol4);
+    }
+
+}
