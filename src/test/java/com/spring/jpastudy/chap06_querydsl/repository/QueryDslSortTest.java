bfe96b6ca6422c36623474d1e5a9c4146db14b26
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-28 09:07:43 +0900

    # 06_08. QueryDslSort Test 01 초기 생성 정렬편

---
+++ src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslSortTest.java
@@ -0,0 +1,66 @@
+package com.spring.jpastudy.chap06_querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.jpastudy.chap06_querydsl.entity.Group;
+import com.spring.jpastudy.chap06_querydsl.entity.Idol;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static com.spring.jpastudy.chap06_querydsl.entity.QIdol.idol;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+// @Rollback(false)
+class QueryDslSortTest {
+
+    @Autowired IdolRepository idolRepository;
+    @Autowired GroupRepository groupRepository;
+
+    // JPAQueryFactory 는 JPA 엔터티를 대상으로 복잡한 쿼리를 간결하고 안전하게 작성할 수 있도록 도와준다.
+    @Autowired
+    JPAQueryFactory factory;
+
+    @BeforeEach
+    void setUp () {
+        //given
+        Group leSserafim = new Group("르세라핌");
+        Group ive = new Group("아이브");
+
+        groupRepository.save(leSserafim);
+        groupRepository.save(ive);
+
+        Idol idol1 = new Idol("김채원", 24, leSserafim);
+        Idol idol2 = new Idol("사쿠라", 26, leSserafim);
+        Idol idol3 = new Idol("가을", 22, ive);
+        Idol idol4 = new Idol("리즈", 20, ive);
+        Idol idol5 = new Idol("장원영", 20, ive);
+
+        idolRepository.save(idol1);
+        idolRepository.save(idol2);
+        idolRepository.save(idol3);
+        idolRepository.save(idol4);
+        idolRepository.save(idol5);
+    }
+
+    @Test
+    @DisplayName("dummyTest")
+    void dummyTest() {
+        // gwt 패턴
+        //given - 테스트에 주어질 데이터
+
+        //when - 테스트 상황
+
+        //then - 테스트 결과 단언
+    }
+
+
+}
