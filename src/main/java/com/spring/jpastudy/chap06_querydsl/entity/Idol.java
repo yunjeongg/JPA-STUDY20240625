8a9a670f118af51f002ddb4a93f5b63e93995ac0
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-28 10:46:14 +0900

    # 06_13. QueryDslGroupingTest Test 01 초기 생성 group by, having

--- src/main/java/com/spring/jpastudy/chap06_querydsl/entity/Idol.java
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/entity/Idol.java
@@ -21,6 +21,8 @@ public class Idol {
 
     private int age;
 
+    private String gender; // 성별 추가
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
@@ -33,6 +35,16 @@ public class Idol {
             changeGroup(group);
         }
     }
+
+    public Idol(String idolName, int age, String gender, Group group) {
+        this.idolName = idolName;
+        this.age = age;
+        this.gender = gender;
+        if (group != null) {
+            changeGroup(group);
+        }
+    }
+
 
     public void changeGroup(Group group) {
         this.group = group;
