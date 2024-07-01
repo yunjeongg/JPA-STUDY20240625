fc7c682f45422ba8ca100da30e66f7ce1d5e9988
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-02 08:47:00 +0900

    # 06_17. QueryDslJoinTest Test 01 초기 생성

---
+++ src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslJoinTest.java
@@ -0,0 +1,89 @@
+package com.spring.jpastudy.chap06_querydsl.repository;
+
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.jpastudy.chap06_querydsl.dto.GroupAverageAgeDto;
+import com.spring.jpastudy.chap06_querydsl.entity.Group;
+import com.spring.jpastudy.chap06_querydsl.entity.Idol;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.spring.jpastudy.chap06_querydsl.entity.QIdol.idol;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+@Transactional
+// @Rollback(false)
+class QueryDslJoinTest {
+
+    @Autowired IdolRepository idolRepository;
+    @Autowired GroupRepository groupRepository;
+
+    // JPAQueryFactory 는 JPA 엔터티를 대상으로 복잡한 쿼리를 간결하고 안전하게 작성할 수 있도록 도와준다.
+    @Autowired
+    JPAQueryFactory factory;
+
+    @BeforeEach
+    void setUp () {
+        //given
+        Group leSserafim = new Group("르세라핌");
+        Group ive = new Group("아이브");
+        Group bts = new Group("방탄소년단");
+        Group newjeans = new Group("뉴진스");
+
+        groupRepository.save(leSserafim);
+        groupRepository.save(ive);
+        groupRepository.save(bts);
+        groupRepository.save(newjeans);
+
+        Idol idol1 = new Idol("김채원", 24, "여", leSserafim);
+        Idol idol2 = new Idol("사쿠라", 26, "여", leSserafim);
+        Idol idol3 = new Idol("가을", 22, "여", ive);
+        Idol idol4 = new Idol("리즈", 20, "여", ive);
+        Idol idol5 = new Idol("장원영", 20, "여", ive);
+        Idol idol6 = new Idol("안유진", 21, "여", ive);
+        Idol idol7 = new Idol("카즈하", 21, "여", leSserafim);
+        Idol idol8 = new Idol("RM", 29, "남", bts);
+        Idol idol9 = new Idol("정국", 26, "남", bts);
+        Idol idol10 = new Idol("해린", 18, "여", newjeans);
+        Idol idol11 = new Idol("혜인", 16, "여", newjeans);
+        Idol idol12 = new Idol("김종국", 48, "남", null); // 그룹 없음
+        Idol idol13 = new Idol("아이유", 31, "여", null); // 그룹 없음
+
+
+        idolRepository.save(idol1);
+        idolRepository.save(idol2);
+        idolRepository.save(idol3);
+        idolRepository.save(idol4);
+        idolRepository.save(idol5);
+        idolRepository.save(idol6);
+        idolRepository.save(idol7);
+        idolRepository.save(idol8);
+        idolRepository.save(idol9);
+        idolRepository.save(idol10);
+        idolRepository.save(idol11);
+        idolRepository.save(idol12);
+        idolRepository.save(idol13);
+
+    }
+
+    @Test
+    @DisplayName("dummyTest")
+    void dummyTest() {
+        // gwt 패턴
+        //given - 테스트에 주어질 데이터
+
+        //when - 테스트 상황
+
+        //then - 테스트 결과 단언
+    }
+}
