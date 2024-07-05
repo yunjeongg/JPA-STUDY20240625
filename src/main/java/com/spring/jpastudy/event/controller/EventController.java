61afa05b02944788d88305ed23448eb4d091508e
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-05 11:33:58 +0900

    # 07_13. Event 스켈레톤페이지 로딩 설정

--- src/main/java/com/spring/jpastudy/event/controller/EventController.java
+++ src/main/java/com/spring/jpastudy/event/controller/EventController.java
@@ -6,6 +6,7 @@ import com.spring.jpastudy.event.dto.response.EventOneDto;
 import com.spring.jpastudy.event.entity.Event;
 import com.spring.jpastudy.event.service.EventService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,12 +23,17 @@ public class EventController {
     private final EventService eventService;
 
     // 전체 조회 요청
+    @SneakyThrows
     @GetMapping("/page/{pageNo}")
     public ResponseEntity<?> getList(
                                     @RequestParam(required = false) String sort,
                                     @PathVariable int pageNo) {
 
         List<EventDetailDto> events = eventService.getEvents(pageNo, sort);
+
+        // 의도적으로 2초간의 로딩을 설정
+        Thread.sleep(2000);
+
         return ResponseEntity.ok().body(events);
     }
 
